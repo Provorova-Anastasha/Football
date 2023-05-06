d55209975ce3bab369a32bd1334937b2efec3cce
unknown <provorova_nastya@mail.ru>
2023-05-06 15:42:16 +0400

    F-03.Реализация пагинации

--- src/components/CardCompetitions/CardCompetitions.jsx
+++ src/components/CardCompetitions/CardCompetitions.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const CardCompetitions = ({ name, area }) => {
   return (
-    <div className='Cardcompetitions'>
+    <div className='cardcompetitions'>
       <Link to  ='/' className="CardCompetitionsButton">
       <h1 className='cards'>{name}</h1>
       <h2 className='cards'>{area}</h2>
